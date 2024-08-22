ad386f2854614bebd57e1d1e06e9403886c4beb6
KFCINEMASOWNER <ujwalnayak25@gmail.com>
2024-08-22 09:22:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ API_HASH = environ.get('API_HASH', 'd5850aeef7dd3d01fe6b698c0a0d4be8')
 if len(API_HASH) == 0:
     print('Error - API_HASH is missing, exiting now')
     exit()
-BOT_TOKEN = environ.get('BOT_TOKEN', '7479470521:AAGG7ejqOr4Jv-zGo1jCVHfCURasJv9_F2w')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7541410743:AAE4nCFqtAig8hn4qOcQyDAEg2PED9RiZNI')
 if len(BOT_TOKEN) == 0:
     print('Error - BOT_TOKEN is missing, exiting now')
     exit()
@@ -113,7 +113,7 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ujwalmayak@fam')
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002163064206")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002228968476")
 if len(BIN_CHANNEL) == 0:
     print('Error - BIN_CHANNEL is missing, exiting now')
     exit()
